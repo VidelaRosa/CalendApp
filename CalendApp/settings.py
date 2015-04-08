c61ed65f280decbe4aee779a82a8f3694ed83bf6
VidelaRosa <vicdelarosa92@gmail.com>
2015-04-09 00:52:37 +0200

    copyright added

--- CalendApp/settings.py
+++ CalendApp/settings.py
@@ -1,4 +1,20 @@
 """
+Copyright 2015 Victor de la Rosa Sanchez
+This file is part of CalendApp.
+
+    CalendApp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    CalendApp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CalendApp.  If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
 Django settings for CalendApp project.
 
 For more information on this file, see
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # CalendApp
 Web Calendar build with Django, Bootstrap and JQuery
 
+Copyright 2015 Victor de la Rosa Sanchez
+vdelarosa@outlook.es
+
+
 ![alt tag](http://s24.postimg.org/ii1ufv36d/Captura_de_pantalla_2015_04_01_a_las_15_24_10.png)
 
 ![alt tag](http://s24.postimg.org/g20120l3p/Captura_de_pantalla_2015_04_01_a_las_15_23_19.png)
